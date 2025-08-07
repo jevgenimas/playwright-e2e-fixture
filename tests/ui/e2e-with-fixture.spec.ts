68aa52e6bb139f4e627e36e1cf178cc0c9a8ace4
Jevgeni <jevgeni.massalov@gmail.com>
2025-08-07 14:35:15 +0300

    Lesson 23

--- tests/ui/e2e-with-fixture.spec.ts
+++ tests/ui/e2e-with-fixture.spec.ts
@@ -35,3 +35,10 @@ test('search for an existing order created through API with fixture', async ({
   await page.getByTestId('searchOrder-submitButton').click()
   await expect(page.getByText('OPEN')).toBeVisible()
 })
+
+test('search for order with delivered status using mock fixture', async ({ mainPage }) => {
+  await mainPage.getByTestId('openStatusPopup-button').click()
+  await mainPage.getByTestId('searchOrder-input').fill('9999')
+  await mainPage.getByTestId('searchOrder-submitButton').click()
+  await expect(mainPage.getByText('DELIVERED', { exact: true })).toBeVisible()
+})
