4336bdc1ddcd0ac5a2352efb0100aad040cea931
Jevgeni <jevgeni.massalov@gmail.com>
2025-08-25 18:05:07 +0300

    Added code to file delivery.fixture.ts . Added test 'search for order with delivered status using mock fixture' to file e2e-with-fixture.spec.ts.

--- tests/ui/e2e-with-fixture.spec.ts
+++ tests/ui/e2e-with-fixture.spec.ts
@@ -5,7 +5,6 @@ import { faker } from '@faker-js/faker/locale/ar'
 test('create order through UI with fixture', async ({ context, auth }) => {
   const page = await context.newPage()
 
-  // Set JWT in localStorage
   await context.addInitScript((token) => {
     localStorage.setItem('jwt', token)
   }, auth.jwt)
@@ -22,16 +21,21 @@ test('search for an existing order created through API with fixture', async ({
   auth,
   orderId,
 }) => {
-  // Set JWT in localStorage
   await context.addInitScript((token) => {
     localStorage.setItem('jwt', token)
   }, auth.jwt)
 
-  // Search for the created order through the UI
   const page = await context.newPage()
   await page.goto(SERVICE_URL)
   await page.getByTestId('openStatusPopup-button').click()
   await page.getByTestId('searchOrder-input').fill(String(orderId))
   await page.getByTestId('searchOrder-submitButton').click()
   await expect(page.getByText('OPEN')).toBeVisible()
+})
+
+test('search for order with delivered status using mock fixture', async ({ mainPage }) => {
+  await mainPage.getByTestId('openStatusPopup-button').click()
+  await mainPage.getByTestId('searchOrder-input').fill('9999')
+  await mainPage.getByTestId('searchOrder-submitButton').click()
+  await expect(mainPage.getByText('DELIVERED', { exact: true })).toBeVisible()
 })
