af4386037c0408dd579d8d8e896a15b09d33d8ed
Jevgeni <jevgeni.massalov@gmail.com>
2025-08-25 18:15:38 +0300

    Command executed: npx prettier --write.

--- tests/fixtures/delivery.fixture.ts
+++ tests/fixtures/delivery.fixture.ts
@@ -1,16 +1,16 @@
-import { test as base } from '@playwright/test'
-import { BASE_API, loginPath, orderPath } from '../../config/env-data'
+import { type Page, test as base } from '@playwright/test'
+import { BASE_API, loginPath, orderPath, SERVICE_URL } from '../../config/env-data'
 import { LoginDto } from '../dto/login-dto'
 import { OrderDto } from '../dto/order-dto'
 
 type Fixtures = {
   auth: { jwt: string }
   orderId: string
+  mainPage: Page
 }
 
 export const test = base.extend<Fixtures>({
   auth: async ({ request }, use) => {
-    // Authorization fixture: Fetch JWT and provide it
     console.log('Init: getting jwt')
     const response = await request.post(`${BASE_API}${loginPath}`, {
       data: LoginDto.createLoginWithCorrectData(),
@@ -21,7 +21,6 @@ export const test = base.extend<Fixtures>({
   },
 
   orderId: async ({ auth, request }, use) => {
-    // Order ID fixture: Create an order and expose the orderId
     const response = await request.post(`${BASE_API}${orderPath}`, {
       data: OrderDto.createOrderWithoutId(),
       headers: {
@@ -33,6 +32,31 @@ export const test = base.extend<Fixtures>({
     const orderId = responseData.id
     console.log('order created with id: ', orderId)
     await use(orderId)
+  },
+
+  mainPage: async ({ context, auth }, use) => {
+    const mainPage = await context.newPage()
+    await mainPage.goto(SERVICE_URL)
+    await context.addInitScript((token) => {
+      localStorage.setItem('jwt', token)
+    }, auth.jwt)
+    await mainPage.reload()
+
+    await mainPage.route(`${BASE_API}${orderPath}/*`, (route) =>
+      route.fulfill({
+        status: 200,
+        contentType: 'application/json',
+        body: JSON.stringify({
+          status: 'DELIVERED',
+          courierId: null,
+          customerName: 'mocked customer',
+          customerPhone: '99887766',
+          comment: '',
+          id: 9999,
+        }),
+      }),
+    )
+    await use(mainPage)
   },
 })
 
